878341119a2ae65f7a3834c10c2bf76de9ceba4c
Joel <thatjoelallison@gmail.com>
2021-10-22 18:54:22 +0100

    Working text-based version of the game... but lacking option to specify board size and treasure-likeliness, and also input validation.

--- src/com/joelallison/Main.java
+++ src/com/joelallison/Main.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void printBoard(int width, int height, String[][] board) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (board[i][j] == null){
-                    board[i][j] = "-";
+    static void printBoard(int width, int height, String[][] board, Boolean[][] knownSpots) {
+        System.out.println("");
+        System.out.print("  ");
+        for (int i = 1; i <= width; i++) {
+            System.out.print(" " + i + " ");
+        }System.out.println("");
+        for (int i = 0; i < height; i++) {
+            System.out.print(i+1 + " ");
+            for (int j = 0; j < width; j++) {
+                if (knownSpots[i][j] == true){
+                    if (board[i][j] == null){
+                        board[i][j] = "x";
+                    }
+                    System.out.print("[" + board[i][j] + "]");
+                }else{
+                    System.out.print("[-]");
                 }
-                System.out.print("[" + board[i][j] + "] ");
             }
             System.out.println("");
         }
@@ -27,14 +37,23 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        final int boardWidth = 10;
-        final int boardHeight = 8;
+        final int boardWidth = 9;
+        final int boardHeight = 9;
         String[][] board = new String[boardWidth][boardHeight];
+        Boolean[][] known = new Boolean[boardWidth][boardHeight];
+
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardHeight; j++) {
+                known[i][j] = false;
+            }
+        }
 
         //amount of treasure is 25% of board size
         final int amountOfTreasure = (int) ((boardWidth * boardHeight) * 0.25);
         final int maxCoins = 9;
         int playerCoins = 0;
+        // declared as -1 so that point 0,0 isn't checked and used to increment player score
+        int[] playerGuess = new int[]{-1,-1};
 
         //put treasure onto board
         for (int i = 0; i < amountOfTreasure; i++) {
@@ -43,13 +62,33 @@ public class Main {
         }
 
         //convoluted way of doing padding d(^-^)
-        System.out.println(new String(new char[(boardWidth/2)-1]).replace("\0", " ") + "--> TREASURE HUNT <--\n");
+        System.out.println(new String(new char[(boardWidth/2)-1]).replace("\0", " ") + "  --> TREASURE HUNT <--\n");
+        System.out.println("This is your board:");
 
-        boolean playing = false;
         //gameloop
-        do{
-            printBoard(boardWidth, boardHeight, board);
-        }while(playing);
+        while (true){
+            System.out.println("");
+
+            if(playerGuess[0] != -1  && board[playerGuess[1]][playerGuess[0]] != null){
+                System.out.println("You found treasure! Your score increased by " + board[playerGuess[1]][playerGuess[0]]);
+                playerCoins += Integer.parseInt(board[playerGuess[1]][playerGuess[0]]);
+            }System.out.println("Score: " + playerCoins);
+
+            printBoard(boardWidth, boardHeight, board, known);
+
+
+
+            System.out.println("\n(type 'q' to quit if you want... but otherwise...)\n\nGuess coordinates [1-9 either axis, formatted 'xy']:");
+            String storedGuess = input.next();
+            if (storedGuess.equalsIgnoreCase(("q"))){break;}
+
+            playerGuess[0] = Integer.parseInt(String.valueOf(storedGuess.charAt(0))) - 1;
+            playerGuess[1] = Integer.parseInt(String.valueOf(storedGuess.charAt(1))) - 1;
+
+            known[playerGuess[1]][playerGuess[0]] = true;
+
+
+        }
 
 
     }
